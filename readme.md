6240bbbb01a65e4ec14d829612849d1152d05496
root <root@s25453.vps.hosting>
2024-04-11 09:13:34 +0000

    finally deploy

--- readme.md
+++ readme.md
@@ -25,3 +25,15 @@ The base logic of Tonsbridge is simple : swap your assert on other chains into a
 - Brdige it to target chain&wallet .
 
 All steps are decentralized .
+
+## Contract address :
+
+Contract address that you should knows
+
+### BSC chain
+
+```
+0xE19618aEFAAC7838E0A8fb084aFbbceb5DE7E14e
+```
+
+Using `0x10ed43c718714eb63d5aa57b78b54704e256024e` as univ2 , and `0x582d872a1b094fc48f5de31d3b73f2d9be47def1` as WTON
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,13 +6,15 @@ async function main() {
     console.log("Account balance:", (await deployer.getBalance()).toString());
   
     const Data = await ethers.getContractFactory("tonsbridge");
-    const data = await Data.connect(deployer).deploy();
-
     //BSC deployed
-    await data.deployed('0x582d872a1b094fc48f5de31d3b73f2d9be47def1','0x10ed43c718714eb63d5aa57b78b54704e256024e');
-
+    const data = await Data.connect(deployer).deploy('0x582d872a1b094fc48f5de31d3b73f2d9be47def1','0x10ed43c718714eb63d5aa57b78b54704e256024e');
     //ETH deployed
-    // await data.deployed('0x76A797A59Ba2C17726896976B7B3747BfD1d220f','0x7a250d5630b4cf539739df2c5dacb4c659f2488d');
+    // const data = await Data.connect(deployer).deploy('0x76A797A59Ba2C17726896976B7B3747BfD1d220f','0x7a250d5630b4cf539739df2c5dacb4c659f2488d');
+
+
+    await data.deployed();
+
+
 
     console.log("PaymentRouter Deployed~!")
     console.log("PaymentRouter Contract address:", data.address);
